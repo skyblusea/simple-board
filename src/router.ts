7ce7b0433919fd2d3fa65ae6858d8f0dbde3e2f8
skyblusea <skyyblusea@gmail.com>
2026-01-23 03:40:55 +0900

    chore: 페이지 경로 수정

--- src/router.ts
+++ src/router.ts
@@ -2,9 +2,9 @@ import { createBrowserRouter } from "react-router";
 
 import { ProtectedRouter } from "@/components/auth/ProtectedRouter";
 import { RootLayout } from "@/components/layout/RootLayout";
-import { LoginPage } from "@/pages/auth/LoginPage";
-import { SignUpPage } from "@/pages/auth/SignUpPage";
 import { HomePage } from "@/pages/home";
+import { LoginPage } from "@/pages/login/LoginPage";
+import { SignUpPage } from "@/pages/signup/SignUpPage";
 
 export const routes = createBrowserRouter([
   {
