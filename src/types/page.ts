e07cb23ff4b609da8161b0419191441aaf4aa60d
skyblusea <skyyblusea@gmail.com>
2026-01-24 05:34:40 +0900

    feat: 페이지네이션 공용 타입 정의

---
+++ src/types/page.ts
@@ -0,0 +1,28 @@
+export interface Sort {
+  unsorted: boolean;
+  sorted: boolean;
+  empty: boolean;
+}
+
+export interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  unpaged: boolean;
+  paged: boolean;
+}
+
+export interface PageResponse<T> {
+  content: T[];
+  pageable: Pageable;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  numberOfElements: number;
+  size: number;
+  number: number;
+  sort: Sort;
+  first: boolean;
+  empty: boolean;
+}
