9e19d8e61d97e0bb13d73f3ac02a67d42e7168ee
skyblusea <skyyblusea@gmail.com>
2026-01-23 15:43:13 +0900

    feat: 토스트 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { RouterProvider } from "react-router";
+
+import { Toaster } from "@/components/ui/sonner";
+
 import { routes } from "./router";
 
 export default function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <>
+      <RouterProvider router={routes} />
+      <Toaster />
+    </>
+  );
 }
