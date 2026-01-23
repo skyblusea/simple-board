8346ce30a19f86f80a3e46a95161cad4b687d773
skyblusea <skyyblusea@gmail.com>
2026-01-23 18:52:13 +0900

    Revert "fix: cors 해결을 위한 임시 proxy 설정"
    This reverts commit 8f909c3ea09bbaa67017ed5e815a4b2cabdda9c9.

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_PROXY_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-          secure: false,
-        },
-      },
-    },
-  };
+  },
 });
