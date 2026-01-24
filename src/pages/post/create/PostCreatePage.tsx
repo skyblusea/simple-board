76663753920654d6d97eedcab40b71adeaf35459
skyblusea <skyyblusea@gmail.com>
2026-01-25 04:48:39 +0900

    refactor: 생성페이지 에러바운더리 설정

--- src/pages/post/create/PostCreatePage.tsx
+++ src/pages/post/create/PostCreatePage.tsx
@@ -1,12 +1,18 @@
 import { FormProvider, useForm } from "react-hook-form";
+import { Navigate } from "react-router";
 
+import { toast } from "sonner";
+
+import { withAsyncBoundary } from "@/components/hoc/withAsyncBoundary";
 import { Page } from "@/components/layout/Page";
+import { Button } from "@/components/ui/button";
 import { PostForm } from "@/pages/post/_component/PostForm";
 import type { PostFormData } from "@/services/post/types";
 
+import { PostFormSkeleton } from "../_component/PostFormSkeleton";
 import { PostCreateButton } from "./PostCreateButton";
 
-export function PostCreatePage() {
+export function PageComponent() {
   const methods = useForm<PostFormData>({
     defaultValues: {
       request: {
@@ -26,3 +32,21 @@ export function PostCreatePage() {
     </FormProvider>
   );
 }
+
+export const PostCreatePage = withAsyncBoundary(PageComponent, {
+  onError: () => toast.error("카테고리를 불러오는데 실패하였습니다."),
+  fallbackError: <Navigate to="/" />,
+  fallbackLoading: (
+    <Page
+      title="게시글 생성"
+      titleAction={
+        <Button disabled className="px-4">
+          생성
+        </Button>
+      }
+      showBackButton={true}
+    >
+      <PostFormSkeleton />
+    </Page>
+  ),
+});
