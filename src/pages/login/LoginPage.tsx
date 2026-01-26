23b3bf791fb96d88b966f08ac6d85849102b388c
skyblusea <skyyblusea@gmail.com>
2026-01-27 01:52:14 +0900

    refactor: 불필요한 html depth 제거

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 
 import { Button, LinkButton } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Field, FieldError, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { useAuth } from "@/contexts/auth/hooks";
 
@@ -45,31 +45,25 @@ export function LoginPage() {
       <Card className="w-full max-w-md">
         <form onSubmit={handleLogin}>
           <CardContent>
-            <FieldGroup>
-              <FieldSet>
-                <FieldGroup>
-                  <Field>
-                    <FieldLabel htmlFor="username">이메일 아이디</FieldLabel>
-                    <Input
-                      id="username"
-                      placeholder="이메일 아이디를 입력해주세요."
-                      {...register("username")}
-                    />
-                    <FieldError>{errors.username?.message}</FieldError>
-                  </Field>
-                  <Field>
-                    <FieldLabel htmlFor="password">비밀번호</FieldLabel>
-                    <Input
-                      id="password"
-                      type="password"
-                      placeholder="비밀번호를 입력해주세요."
-                      {...register("password")}
-                    />
-                    <FieldError>{errors.password?.message}</FieldError>
-                  </Field>
-                </FieldGroup>
-              </FieldSet>
-            </FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="username">이메일 아이디</FieldLabel>
+              <Input
+                id="username"
+                placeholder="이메일 아이디를 입력해주세요."
+                {...register("username")}
+              />
+              <FieldError>{errors.username?.message}</FieldError>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="password">비밀번호</FieldLabel>
+              <Input
+                id="password"
+                type="password"
+                placeholder="비밀번호를 입력해주세요."
+                {...register("password")}
+              />
+              <FieldError>{errors.password?.message}</FieldError>
+            </Field>
           </CardContent>
           <CardFooter className="mt-7 flex flex-col space-y-2">
             <Button type="submit" className="w-full">
--- src/pages/signup/Page.tsx
+++ src/pages/signup/Page.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 
 import { Button, LinkButton } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Field, FieldError, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { useAuth } from "@/contexts/auth/hooks";
 
@@ -49,46 +49,40 @@ export function SignupPage() {
       <Card className="w-full max-w-md">
         <form onSubmit={handleSignup}>
           <CardContent>
-            <FieldGroup>
-              <FieldSet>
-                <FieldGroup>
-                  <Field>
-                    <FieldLabel htmlFor="username">이메일 아이디</FieldLabel>
-                    <Input
-                      id="username"
-                      placeholder="이메일 아이디를 입력해주세요."
-                      {...register("username")}
-                    />
-                    <FieldError>{errors.username?.message}</FieldError>
-                  </Field>
-                  <Field>
-                    <FieldLabel htmlFor="password">비밀번호</FieldLabel>
-                    <Input
-                      id="password"
-                      type="password"
-                      placeholder="영문, 숫자, 특수문자(!%*#?&) 조합 8자 이상"
-                      {...register("password")}
-                    />
-                    {errors.password && <FieldError>{errors.password.message}</FieldError>}
-                  </Field>
-                  <Field>
-                    <FieldLabel htmlFor="confirmPassword">비밀번호 확인</FieldLabel>
-                    <Input
-                      id="confirmPassword"
-                      type="password"
-                      placeholder="비밀번호를 다시 입력해주세요."
-                      {...register("confirmPassword")}
-                    />
-                    <FieldError>{errors.confirmPassword?.message}</FieldError>
-                  </Field>
-                  <Field>
-                    <FieldLabel htmlFor="name">이름</FieldLabel>
-                    <Input id="name" placeholder="이름을 입력해주세요" {...register("name")} />
-                    <FieldError>{errors.name?.message}</FieldError>
-                  </Field>
-                </FieldGroup>
-              </FieldSet>
-            </FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="username">이메일 아이디</FieldLabel>
+              <Input
+                id="username"
+                placeholder="이메일 아이디를 입력해주세요."
+                {...register("username")}
+              />
+              <FieldError>{errors.username?.message}</FieldError>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="password">비밀번호</FieldLabel>
+              <Input
+                id="password"
+                type="password"
+                placeholder="영문, 숫자, 특수문자(!%*#?&) 조합 8자 이상"
+                {...register("password")}
+              />
+              <FieldError>{errors.password?.message}</FieldError>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="confirmPassword">비밀번호 확인</FieldLabel>
+              <Input
+                id="confirmPassword"
+                type="password"
+                placeholder="비밀번호를 다시 입력해주세요."
+                {...register("confirmPassword")}
+              />
+              <FieldError>{errors.confirmPassword?.message}</FieldError>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="name">이름</FieldLabel>
+              <Input id="name" placeholder="이름을 입력해주세요" {...register("name")} />
+              <FieldError>{errors.name?.message}</FieldError>
+            </Field>
           </CardContent>
           <CardFooter className="mt-7 flex flex-col space-y-2">
             <Button type="submit" className="w-full">
