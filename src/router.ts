a88be9757c37f27966602abf1a33dbc384a41794
skyblusea <skyyblusea@gmail.com>
2026-01-27 01:52:44 +0900

    refactor: 컴포넌트명 컨벤션 통일

--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,7 @@ import { PostCreatePage } from "@/pages/post/create/PostCreatePage";
 import { PostDetailPage } from "@/pages/post/detail/PostDetailPage";
 import { PostEditPage } from "@/pages/post/edit/PostEditPage";
 import { PostListPage } from "@/pages/post/list/PostListPage";
-import { SignupPage } from "@/pages/signup/Page";
+import { SignupPage } from "@/pages/signup/SignupPage";
 
 export const routes = createBrowserRouter([
   {
